1b11e30f9a9ebf2522a84aa7b9e33be87828dc22
sbdarjun <arzoonsubedi123@gmail.com>
2024-04-23 13:57:52 -0500

    added required dependencies and validate get API

--- src/test/java/validate/GetMethod.java
+++ src/test/java/validate/GetMethod.java
@@ -1,2 +1,20 @@
-package validate;public class getmethod {
+package validate;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GetMethod {
+    @Test
+    public void get() {
+        Response res = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println(res.getStatusCode());
+        System.out.println(res.getTime());
+        System.out.println(res.getBody().asString());
+        System.out.println(res.getStatusLine());
+        System.out.println(res.getHeader("content-type"));
+        int statusCode = res.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+    }
 }
