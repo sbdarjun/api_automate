d0249f1d854eabfcdb8b8ff2313583cab48a898a
sbdarjun <arzoonsubedi123@gmail.com>
2024-04-23 15:26:38 -0500

    Refactor GET API and automated POST API endpoint

--- src/test/java/validate/GetMethod.java
+++ src/test/java/validate/GetMethod.java
@@ -1,20 +1,22 @@
 package validate;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GetMethod {
     @Test
-    public void get() {
-        Response res = RestAssured.get("https://reqres.in/api/users?page=2");
-        System.out.println(res.getStatusCode());
-        System.out.println(res.getTime());
-        System.out.println(res.getBody().asString());
-        System.out.println(res.getStatusLine());
-        System.out.println(res.getHeader("content-type"));
-        int statusCode = res.getStatusCode();
-        Assert.assertEquals(statusCode, 200);
+    public void testGetAPI() {
+        baseURI = "https://reqres.in/api";
+        given().get("/users?page=2").
+                then().
+                statusCode(200).
+                body("data[1].id", equalTo(8))
+                .body("data.email", hasItem("lindsay.ferguson@reqres.in"))
+                .log().all();
     }
 }
---
+++ src/test/java/validate/PostMethod.java
@@ -0,0 +1,28 @@
+package validate;
+
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+public class PostMethod {
+    @Test
+    public void testPostAPI(){
+        baseURI = "https://reqres.in/api";
+        JSONObject request = new JSONObject();
+        request.put("name", "tester");
+        request.put("job","Software QA");
+        given()
+                .header("Content-Type","application/json")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(request.toJSONString())
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(201)
+                .log().all();
+    }
+}
