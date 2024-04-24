9de5cd404887ce9be902cec1d8e4931dac9ae2ce
sbdarjun <arzoonsubedi123@gmail.com>
2024-04-23 22:22:20 -0500

    JSON schema validation

---
+++ src/test/java/validate/JSONSchemaValidation.java
@@ -0,0 +1,20 @@
+package validate;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class JSONSchemaValidation {
+    @Test
+    public void JSONValidator(){
+        baseURI = "https://reqres.in/api";
+        given().
+                get("/users?page=2").
+                then().
+                assertThat().body(matchesJsonSchemaInClasspath("schema.json")).
+                statusCode(200)
+                .log().all();
+    }
+}
